84f703cddfe9e6b46eab5340b58512f64097e0db
Thang Tran <72323340+thangtrantt@users.noreply.github.com>
2024-10-27 10:25:10 +0000

    Pending changes exported from your codespace

--- openvoice/se_extractor.py
+++ openvoice/se_extractor.py
@@ -19,7 +19,8 @@ model = None
 def split_audio_whisper(audio_path, audio_name, target_dir='processed'):
     global model
     if model is None:
-        model = WhisperModel(model_size, device="cuda", compute_type="float16")
+        # model = WhisperModel(model_size, device="cuda", compute_type="float16")
+        model = WhisperModel(model_size, device="cpu", compute_type="float32") # Use device cpu instead cuda
     audio = AudioSegment.from_file(audio_path)
     max_len = len(audio)
 
